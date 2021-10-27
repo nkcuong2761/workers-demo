1967c804381550db54c89cbb30dad003058c9dc0
Cuong Nguyen <nkcuong2761@gmail.com>
2021-10-27 16:57:36 -0400

    yessir

--- src/components/posts.js
+++ src/components/posts.js
@@ -9,7 +9,9 @@ const Posts = () => {
       const resp = await fetch(
         "https://my-worker.ckn006.workers.dev/api/posts"
       )
-      const postsResp = await resp.json()
+      let temp = await resp.json()
+      const postsResp = temp.map(j => JSON.parse(j))
+      console.log('postResp: ',postsResp);
       setPosts(postsResp)
     }
 
@@ -20,10 +22,11 @@ const Posts = () => {
     <div>
       <h1>Posts</h1>
       {posts.map((post) => (
-        <div>
-          <h2>
-            {post.name}
-          </h2>
+        <div key={post.title}>
+          <div><span style={{fontWeight:'bold'}}>title:&nbsp;</span>{post.title}</div><br/>
+          <div><span style={{fontWeight:'bold'}}>username:&nbsp;</span>{post.username}</div><br/>
+          <div><span style={{fontWeight:'bold'}}>content:&nbsp;</span>{post.content}</div><br/>
+          <br/>
         </div>
       ))}
     </div>
